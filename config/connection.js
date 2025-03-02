d4d50f470c8e611447b67d3e71c729635e90d8cd
safeer239 <mohammedsafeershefi@gamil.com>
2025-03-02 17:31:02 +0530

    Fixing deployment issues

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     // console.log(process.env.MONGO_URI);
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI ||"mongodb+srv://mohammedsafeershefi:safeershefi@cluster0.nu9w4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log(`Mongo db connected: ${conn.connection.host}`);
     console.log("Mongo db connected");
   } catch (error) {
