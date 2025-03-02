7933462c1b64c415b7914a6691b99e136bed1bf9
safeer239 <mohammedsafeershefi@gamil.com>
2025-03-02 17:43:11 +0530

    Fixing deployment issues(MongoDB connection)

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
     // console.log(process.env.MONGO_URI);
-    const conn = await mongoose.connect(process.env.MONGO_URI ||"mongodb+srv://mohammedsafeershefi:safeershefi@cluster0.nu9w4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Mongo db connected: ${conn.connection.host}`);
     console.log("Mongo db connected");
   } catch (error) {
