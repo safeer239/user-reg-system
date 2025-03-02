e81dce6c67ce255b1bae0eb75542ff55386de1bc
safeer239 <mohammedsafeershefi@gamil.com>
2025-03-02 17:27:36 +0530

    Fixing deployment issues

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     // console.log(process.env.MONGO_URI);
-    const connection = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Mongo db connected: ${conn.connection.host}`);
     console.log("Mongo db connected");
   } catch (error) {
     console.log("mongo DB connection Failed ");
--- server.js
+++ server.js
@@ -18,4 +18,4 @@ const PORT =process.env.PORT || 8080
 app.listen(PORT,()=>{
     console.log("Server listening on port "+PORT)
 });
- connectDB()
+connectDB()
